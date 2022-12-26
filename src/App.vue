3c8877730ec852ab112bcbe6fc3b3bb877ed3116
Akam <akam-nejati>
2022-12-26 23:17:36 +0330

    responsive and add filter

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,30 @@
 <script setup lang="ts">
-  import Main from './components/Main.vue';
+import { ref } from "vue";
+import Main from "./components/Main.vue";
+import Error from "./components/Error_page.vue";
+
+var userAgent = window.navigator.userAgent;
+const platform = window.navigator.platform;
+const iosPlatforms = ["iPhone", "iPad", "iPod"];
+const os = ref<string | null>(null);
+
+if (iosPlatforms.indexOf(platform) !== -1) {
+  os.value = "iOS";
+} else if (/Android/.test(userAgent)) {
+  os.value = "Android";
+}
 </script>
 
 <template>
-  <div class="w-full h-screen bg-slate-900 flex justify-center items-center"> 
+  <div
+    v-if="!os"
+    class="w-full h-screen bg-slate-900 flex justify-center items-center"
+  >
     <Main />
+  </div>
+  <div v-else>
+    <Error />
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
---
+++ src/components/Error_page.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="w-full h-screen flex justify-center items-center bg-red-500">
+    <p class="p-4 text-gray-300 text-2xl">
+      Sorry, this app is not available for Android and iOS. Please try twice
+      with a PC
+    </p>
+  </div>
+</template>
+
+<style scoped></style>
